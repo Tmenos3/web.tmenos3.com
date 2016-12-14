81a552760fcfa80193f5bff4eb05e8358bc0fce7
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2016-12-14 15:34:41 -0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+//import logo from '../static/logo.svg';
+import photo from '../static/(t-3).png';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+        </div>
+        <img src={photo} />
+      </div>
+    );
+  }
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <div className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <h2>Welcome to React</h2>
+  //       </div>
+  //       <p className="App-intro">
+  //         To get started, edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //     </div>
+  //   );
+  // }
+}
+
+export default App;
